c4caa2179b44ee4a7315b9f6eb6e5e31f68302c1
Chintan desai <chintan.desai@citrix.com>
2016-12-10 20:25:51 -0800

    Consuming youtube api response in a list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,25 @@
 /**
  * Created by chintan on 12/7/16.
  */
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+class VideoList extends React.Component {
+
+    renderVideoItems() {
+        return this.props.videos.map(video => {
+            return <VideoListItem key={video.etag} video={video} />;
+        });
+    }
+
+    render() {
+
+        return (
+            <ul className="col-md-4 list-group">
+                {this.renderVideoItems()}
+            </ul>
+        );
+    }
+}
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'sample_key';
 
@@ -8,12 +10,26 @@ const API_KEY = 'sample_key';
 // Used when you just take some data and spit some JSX out
 // Can have class based component
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+
 };
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
