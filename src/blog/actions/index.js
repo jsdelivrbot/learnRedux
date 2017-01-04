b6723eb76e88f0b1b7160ab1475909c85044de67
Chintan desai <chintan.desai@citrix.com>
2017-01-04 10:58:31 -0800

    Complete add post component with routing changes for submit and cancel actions

--- src/blog/actions/index.js
+++ src/blog/actions/index.js
@@ -4,6 +4,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const ADD_POST = 'ADD_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = "?key=1819sgshjwwm";
@@ -15,4 +16,14 @@ export function fetchPosts() {
         type: FETCH_POSTS,
         payload: request
     };
-};
+}
+
+export function createPost(props) {
+    console.log(props);
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+    return {
+        type: ADD_POST,
+        payload: request
+    }
+}
--- src/blog/components/post_new.js
+++ src/blog/components/post_new.js
@@ -1,14 +1,89 @@
 /**
- * Created by chintan on 1/3/17.
+ * Created by chintan.
  */
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import {reduxForm} from 'redux-form';
+
+import {createPost} from '../actions/index';
+import {Link} from 'react-router';
 
 class AddPosts extends Component {
+
+    // Avoid context to be used in your application as much as possible
+    // React api for context is still in flux and may change in future.
+    // Only use it while working with router for now
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+    onSubmit(props) {
+        this.props.createPost(props)
+            .then(() => {
+                // blog post has been created. navigate the user to the index page
+                // We navigate by calling router.push with the new route to navigate to
+                this.context.router.push("/");
+            });
+    }
     render() {
+        const {fields:{title, categories, content}, handleSubmit} = this.props;
         return (
-            <div>Create a Post</div>
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                    <h3>Create a Post</h3>
+                    <div className={`form-group ${title.touched && title.invalid ? 'has-danger': ''}`}>
+                        <label>Title</label>
+                        <input type="text" className="form-control" {...title}/>
+                        <div className="text-help">
+                            {title.touched ? title.error : ''}
+                        </div>
+                    </div>
+                    <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger': ''}`}>
+                        <label>Categories</label>
+                        <input type="text" className="form-control" {...categories}/>
+                        <div className="text-help">
+                            {categories.touched? categories.error : ''}
+                        </div>
+                    </div>
+                    <div className={`form-group ${content.touched && content.invalid ? 'has-danger': ''}`}>
+                        <label>Content</label>
+                        <textarea className="form-control" {...content}/>
+                        <div className="text-help">
+                            {content.touched? content.error : ''}
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <Link to="/" className="btn btn-danger">Cancel</Link>
+                </form>
         );
     };
 }
 
-export default AddPosts;
+function validate(values) {
+    const errors = {};
+
+    if(!values.title) {
+        errors.title = 'Enter a username';
+    }
+
+    if(!values.categories){
+        errors.categories = 'Enter a category';
+    }
+    if(!values.content){
+        errors.content = 'Enter some content';
+    }
+    return errors;
+}
+
+// For connect - First argument is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm - First is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+export default reduxForm({
+    form: 'AddPostForm',
+    fields: [
+        'title',
+        'categories',
+        'content'
+    ],
+    validate
+},
+    null,
+    {createPost}
+)(AddPosts);
